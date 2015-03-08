6e970f21bdd8e6b7d5f7f441fc28f3ceb61f94c9
Chad Jones <chadj@google.com>
2015-03-08 09:49:56 -0700

    Add option for EXTRA_CMDS to be eval'd after main build
    Change-Id: Ic6ecec699f8fbe23d6b00d4b2be95137d62a5241

--- build.sh
+++ build.sh
@@ -10,11 +10,12 @@
 
 set -e
 
-ROOT_DIR=$(readlink -f $(dirname $0)/..)
+export ROOT_DIR=$(readlink -f $(dirname $0)/..)
+export MAKE_ARGS=$@
 
 . ${ROOT_DIR}/build.config
-OUT_DIR=$(readlink -m ${OUT_DIR:-${ROOT_DIR}/out/${BRANCH}})
-DIST_DIR=$(readlink -m ${DIST_DIR:-${OUT_DIR}/dist})
+export OUT_DIR=$(readlink -m ${OUT_DIR:-${ROOT_DIR}/out/${BRANCH}})
+export DIST_DIR=$(readlink -m ${DIST_DIR:-${OUT_DIR}/dist})
 
 export PATH=${ROOT_DIR}/${LINUX_GCC_CROSS_COMPILE_PREBUILTS_BIN}:${PATH}
 cd ${ROOT_DIR}
@@ -22,14 +23,26 @@ cd ${ROOT_DIR}
 mkdir -p ${OUT_DIR}
 echo "========================================================"
 echo " Setting up for build"
+set -x
 (cd ${KERNEL_DIR} && \
  make O=${OUT_DIR} ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} ${DEFCONFIG} && \
  make mrproper)
+set +x
 
 echo "========================================================"
 echo " Building kernel"
+set -x
 (cd ${OUT_DIR} && \
  make O=${OUT_DIR} ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} -j8 $@)
+set +x
+
+if [ "${EXTRA_CMDS}" != "" ]; then
+  echo "========================================================"
+  echo " Running extra build command(s):"
+  set -x
+  eval ${EXTRA_CMDS}
+  set +x
+fi
 
 mkdir -p ${DIST_DIR}
 echo "========================================================"
