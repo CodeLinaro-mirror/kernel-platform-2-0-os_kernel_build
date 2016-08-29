710746c697821b6ec08a1de6cf450666f7855547
Lorenzo Colitti <lorenzo@google.com>
2016-08-29 12:34:48 +0900

    Pass the branch name to the kernel tests.
    This will allow the kernel tests to skip certain tests depending
    on what branch they are running on. This is useful in the case of
    security backport kernel branches, which do not get new features
    and may not get bug fixes.
    
    Change-Id: I9f6962bd9718ac780c8e3fc8ad22e82f83c5fc09

--- build_test.sh
+++ build_test.sh
@@ -10,12 +10,16 @@ export BUILD_CONFIG=build/build.config.net_test
 
 test=all_tests.sh
 set -e
+# Source the per-device build config in order to set $BRANCH.
+# envsetup.sh will later source build.config.net_test, which
+# will overwrite the variables we care about.
+source ${ROOT_DIR}/build.config
 source ${ROOT_DIR}/envsetup.sh
 
 echo "========================================================"
 echo " Building kernel and running tests "
 
 cd ${KERNEL_DIR}/*
-$NET_TEST/run_net_test.sh --builder $test
+$NET_TEST/run_net_test.sh --builder --branch $BRANCH $test
 echo $?
 echo "======Finished running tests======"
