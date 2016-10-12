a2a6a6a0df064bef1364a2b5b92a611445cf53c2
Nick Desaulniers <ndesaulniers@google.com>
2016-10-12 16:49:15 -0700

    buildinfo: fix up src include paths
    When a *.cmd file specifies a -I arg that's not an absolute path,
    specify absolute paths to the kernel source directory AND the out
    directory, not just the out directory.  This is what BEAR does.
    
    Change-Id: I3359e17cbe2042a885080ecb37c4acda4c764c23

--- buildinfo/buildinfo.py
+++ buildinfo/buildinfo.py
@@ -21,7 +21,8 @@ units speeds up analysis by only looking at files included in this build.  The
 includes and defines flag gives us information required for producting a
 correct AST.
 
-usage: OUT_DIR=/path/to/out/branch/ python buildinfo.py
+usage: ROOT_DIR=/path/to/kernel KERNEL_DIR=private/<chipset>-<vendor> \
+       OUT_DIR=/path/to/out/branch/ python buildinfo.py
 """
 
 import json
@@ -45,8 +46,12 @@ def parse_cmd_files(cmds):
 
 def make_abs(s):
   if s[2] != os.sep:
-    return "-I" + os.path.join(os.environ["OUT_DIR"], s[2:])
-  return s
+    return [
+        "-I" + os.path.join(os.environ["OUT_DIR"], s[2:]),
+        "-I" + os.path.join(os.environ["ROOT_DIR"], os.environ["KERNEL_DIR"],
+                            s[2:])
+    ]
+  return [s]
 
 
 def filter_flags(all_flags):
@@ -55,7 +60,7 @@ def filter_flags(all_flags):
     if flag.startswith("-D") or flag == "-nostdinc":
       flags.append(flag)
     elif flag.startswith("-I"):
-      flags.append(make_abs(flag))
+      flags.extend(make_abs(flag))
     elif flag == "-include" or flag == "-isystem":
       flags.extend(all_flags[i:i + 2])
   return flags
@@ -73,7 +78,7 @@ def parse_cmd_file(cmd):
 def write_out(db):
   path = os.path.join(os.environ["OUT_DIR"], "compile.json")
   with open(path, "w") as fp:
-    fp.write(json.dumps(db, indent=4))
+    fp.write(json.dumps(db, indent=4, separators=(",", ": ")))
 
 if __name__ == "__main__":
   write_out(parse_cmd_files(find_cmd_files()))
