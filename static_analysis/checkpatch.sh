2dc476a175414a2ba8877828d5b0621d173f4aba
Mike Wallstedt <mikewallstedt@google.com>
2017-01-05 20:04:31 -0800

    Adds --repo_prop. Checks range of changes after given sha1.
    --repo_prop is useful when you know the name of the repo before hand,
    but not the sha1 (i.e. as in presubmit).
    
    It's better to check the range of patches after the given sha1, since
    the value given by repo.prop may refer to the oldest change in a stack.
    
    As as a side effect, the patches are written to files. This enables
    checkpatch.pl to also warn about errors in the change description,
    whereas it (apparently) fails to do so if the patch is passed on stdin.
    
    Change-Id: I7ac0b82140608b80409bb685575c48ea882e5c1d

--- static_analysis/checkpatch.sh
+++ static_analysis/checkpatch.sh
@@ -11,10 +11,14 @@ export OUT_DIR=$(readlink -m ${OUT_DIR:-${ROOT_DIR}/out/${BRANCH}})
 export DIST_DIR=$(readlink -m ${DIST_DIR:-${OUT_DIR}/dist})
 mkdir -p ${DIST_DIR}
 
+# Save the original value before converting to abs path.
+REPO_PATH=${KERNEL_DIR}
 export KERNEL_DIR=$(readlink -m ${KERNEL_DIR})
 
 CHECKPATCH_PL_PATH="${KERNEL_DIR}/scripts/checkpatch.pl"
 GIT_SHA1="HEAD"
+PATCH_DIR="${OUT_DIR}/patches"
+CHECKED_DIR="${OUT_DIR}/checked"
 BLACKLIST_FILE="${STATIC_ANALYSIS_SRC_DIR}/checkpatch_blacklist"
 RESULTS_PATH=${DIST_DIR}/checkpatch_violations.txt
 
@@ -22,8 +26,21 @@ RESULTS_PATH=${DIST_DIR}/checkpatch_violations.txt
 CHECKPATCH_ARGS=()
 while [[ $# -gt 0 ]]; do
   next="$1"
-  case ${next} in --git_sha1)
+  case ${next} in
+  --git_sha1)
     GIT_SHA1="$2"
+    shift
+    ;;
+  --repo_prop)
+    if [[ ! -f "$2" ]]; then
+      echo "Failed to find file $2"
+      exit 1
+    fi
+    GIT_SHA1=$(grep -E "${REPO_PATH} [0-9a-f]+" "$2" | awk '{print $2}')
+    if [[ -z "${GIT_SHA1}" ]]; then
+      echo "Failed to find repo ${REPO_PATH} in $2"
+      exit 1
+    fi
     shift
     ;;
   --blacklisted_checks)
@@ -36,6 +53,7 @@ while [[ $# -gt 0 ]]; do
     echo ""
     echo "Usage: $0"
     echo "  <--git_sha1 nnn> (Defaults to HEAD)"
+    echo "  <--repo_prop path> (Gets SHA1 from this file, indexed by KERNEL_DIR)"
     echo "  <--blacklisted_checks path_to_file> (Defaults to checkpatch_blacklist)"
     echo "  <args for checkpatch.pl>"
     exit 0
@@ -48,10 +66,17 @@ while [[ $# -gt 0 ]]; do
 done
 
 
-echo "========================================================"
-echo " Running static analysis..."
-echo "    Using KERNEL_DIR: " ${KERNEL_DIR}
-echo "    Results written to: " ${RESULTS_PATH}
+# Clean up from any previous run.
+if [[ -d "${PATCH_DIR}" ]]; then
+  rm -fr "${PATCH_DIR}"
+fi
+mkdir -p "${PATCH_DIR}"
+
+if [[ -d "${CHECKED_DIR}" ]]; then
+  rm -fr "${CHECKED_DIR}"
+fi
+mkdir -p "${CHECKED_DIR}"
+
 
 # Update blacklist.
 if [[ -f "${BLACKLIST_FILE}" ]]; then
@@ -62,9 +87,26 @@ if [[ -f "${BLACKLIST_FILE}" ]]; then
   fi
 fi
 
+
 # Check the patch for errors.
+echo "========================================================"
+echo " Running static analysis..."
+echo "    Using KERNEL_DIR: ${KERNEL_DIR}"
+
 cd ${KERNEL_DIR}
-git format-patch -1 --stdout "${GIT_SHA1}" | "${CHECKPATCH_PL_PATH}" ${CHECKPATCH_ARGS[*]} - | { grep -E -A1 "^ERROR:" || true; } > ${RESULTS_PATH}
+git format-patch --quiet -o "${PATCH_DIR}" "${GIT_SHA1}^1"
+echo "    Analyzing $(ls -l ${PATCH_DIR} | grep -c [.]patch) commits"
 
+# TODO(mikewallstedt): Run these in parallel?
+for P in ${PATCH_DIR}/*.patch; do
+  O="${CHECKED_DIR}/$(basename ${P%.patch}.checked)"
+  # Ignore return code from checkpatch.pl, since it may be due to violations...
+  ${CHECKPATCH_PL_PATH} ${CHECKPATCH_ARGS[*]} "${P}" > "${O}" || true
+  # but still ensure that it produced a result.
+  ls ${O} > /dev/null
+done
+
+{ grep -r -h -E -A1 "^ERROR:" "${CHECKED_DIR}" || true; } > ${RESULTS_PATH}
+
+echo "    Results written to: " ${RESULTS_PATH}
 echo "======Finished running static analysis.======"
-
