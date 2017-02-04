ce47d3e044a7acc5eb1d5e009fcfd35492658b50
Thierry Strudel <tstrudel@google.com>
2017-02-04 15:29:51 -0800

    change PATH only if LZ4 path is defined and place first in path
    Also print useful information
    
    Change-Id: I5314494a0bbcb18079fecf94e11678b7f6720d42

--- envsetup.sh
+++ envsetup.sh
@@ -13,12 +13,22 @@ export ROOT_DIR=$PWD
 export BUILD_CONFIG=${BUILD_CONFIG:-build.config}
 . ${ROOT_DIR}/${BUILD_CONFIG}
 
+echo "========================================================"
+echo "= build config: ${ROOT_DIR}/${BUILD_CONFIG}"
+cat ${ROOT_DIR}/${BUILD_CONFIG}
+
 # Mitigate dup paths
 PATH=${PATH//"${ROOT_DIR}/${LINUX_GCC_CROSS_COMPILE_PREBUILTS_BIN}:"}
-PATH=${PATH//"${ROOT_DIR}/${LZ4_PREBUILTS_BIN}:"}
-
 export PATH=${ROOT_DIR}/${LINUX_GCC_CROSS_COMPILE_PREBUILTS_BIN}:${PATH}
-PATH=$PATH:${ROOT_DIR}/${LZ4_PREBUILTS_BIN}
+
+if [ ! -z "${LZ4_PREBUILTS_BIN}" ] ; then
+    PATH=${PATH//"${ROOT_DIR}/${LZ4_PREBUILTS_BIN}:"}
+    export PATH=${ROOT_DIR}/${LZ4_PREBUILTS_BIN}:${PATH}
+fi
+
+echo
+echo "PATH=${PATH}"
+echo
 
 export $(sed -n -e 's/\([^=]\)=.*/\1/p' ${ROOT_DIR}/${BUILD_CONFIG})
 
