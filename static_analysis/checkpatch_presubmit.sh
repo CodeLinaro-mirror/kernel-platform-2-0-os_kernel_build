ebbb17aeaab691e914db01923db8dfbe72da81db
Mike Wallstedt <mikewallstedt@google.com>
2017-02-17 12:00:31 -0800

    Change how check_presubmit identifies build_type.
    BUILD_INFO is not available in the dist dir at the time we need it.
    
    Change-Id: Ib0e3610185826257469c4cbd87f70fbd2b1312cc
    (cherry picked from commit 2c2b3bbf2d3411fc439675ab14304a9ea520fa81)

--- static_analysis/checkpatch_presubmit.sh
+++ static_analysis/checkpatch_presubmit.sh
@@ -16,21 +16,48 @@ REPO_PROP_PATH=${DIST_DIR}/repo.prop
 BUILD_INFO_PATH=${DIST_DIR}/BUILD_INFO
 
 verify_file_exists() {
-  if [[ ! -f ${REPO_PROP_PATH} ]]; then
-    echo "Missing ${REPO_PROP_PATH}"
+  if [[ ! -f $1 ]]; then
+    echo "Missing $1"
     exit 1
   fi
 }
 
+# Parse flags.
+BUILD_ID=""
+FORWARDED_ARGS=()
+while [[ $# -gt 0 ]]; do
+  next="$1"
+  case ${next} in
+  --bid)
+    BUILD_ID="$2"
+    shift
+    ;;
+  --help)
+    echo "Checks whether given build is for presubmit. If so, extract git_sha1"
+    echo "from repo.prop and invoke checkpatch.sh."
+    echo ""
+    echo "Usage: $0"
+    echo "  <--bid nnn> (The build ID. Required.)"
+    echo "  <args for checkpatch.sh>"
+    exit 0
+    ;;
+  *)
+    FORWARDED_ARGS+=("$1")
+    ;;
+  esac
+  shift
+done
+
+if [[ -z $BUILD_ID ]]; then
+  echo "WARNING: No --bid supplied. Assuming not presubmit build. Exiting."
+  exit 0
+fi
+
 # Skip checkpatch for postsubmit (b/35390488)
-verify_file_exists ${BUILD_INFO_PATH}
-set +e
-grep -E build_type.*pending "${BUILD_INFO_PATH}"
-if [[ $? -ne 0 ]]; then
+if [[ "P" -ne ${BUILD_ID:0:1} ]]; then
    echo "Did not identify a presubmit build. Exiting."
    exit 0
 fi
-set -e
 
 # Pick the correct patch to test.
 verify_file_exists ${REPO_PROP_PATH}
@@ -40,5 +67,5 @@ if [[ -z ${GIT_SHA1} ]]; then
   exit 1
 fi
 
-${STATIC_ANALYSIS_SRC_DIR}/checkpatch.sh --git_sha1 ${GIT_SHA1} $@
+${STATIC_ANALYSIS_SRC_DIR}/checkpatch.sh --git_sha1 ${GIT_SHA1} ${FORWARDED_ARGS[*]}
 
